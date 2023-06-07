d82e0971bcac51ae601af4a9e369c5c37f280f55
jakebroffman <jake.broffman1088@gmail.com>
2023-06-07 19:40:41 -0400

    commit tree 945f69857c4388a1e9f980935c5c79d856505b3a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  x = 1
+  dwarves.each do |dwarf|
+    puts "#{x}. #{dwarf}"
+    x += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.each do |ingredient|
+    if cheese_types.include?(ingredient)
+      return ingredient
+    end
+  end
+  return nil
 end
